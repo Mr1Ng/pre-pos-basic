282fcf0146193eb2904ecfdf1477028a34679d4f
Mr1Ng <37356837+Mr1Ng@users.noreply.github.com>
2019-08-21 10:24:28 +0800

    Add files via upload

---
+++ practice-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function find(collection, element) {
+    let result;
+    for (let i =0; i<collection.length;i++){
+      if(collection[i]===element){
+        return collection[i];
+      }
+       
+      
+    }
+    return 查无此数;
+}
